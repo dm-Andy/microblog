97e80b2b5b6e7e63aad92b94d1f65ff4faad022b
andy <yuchao635885852@gmail.com>
2019-01-12 22:08:38 +0800

    edit config.py

--- config.py
+++ config.py
@@ -17,8 +17,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'SQLALCHEMY_DATABASE_URI') or 'mysql+pymysql://root:root@localhost:3306/microblog'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # 分页每页数量
     POSTS_PER_PAGE = 5
