a881e957c2602e1add3f3d24ceda9b99c23e60a2
andy <yuchao635885852@gmail.com>
2019-01-16 01:37:08 +0800

    fix a bug in tasks

--- app/tasks.py
+++ app/tasks.py
@@ -43,8 +43,8 @@ def export_posts(user_id):
             _set_task_progress(i*100 // posts_count)
 
         send_email('[Microblog] Your blog posts',
-                [app.config['ADMINS'][0]],
-                user.email,
+                app.config['ADMINS'][0],
+                [user.email],
                 render_template('email/export_posts.txt',user=user),
                 render_template('email/export_posts.html',user=user),
                 [('posts.json', 'application/json',json.dumps(data,indent=4))],
